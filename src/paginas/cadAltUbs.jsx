b7ca068d28eba28f98acf60390609f95eb9f9452
jposawa <jposawa@gmail.com>
2025-04-21 13:45:23 -0300

    Corrijindo formulário

--- src/paginas/cadAltUbs.jsx
+++ src/paginas/cadAltUbs.jsx
@@ -14,10 +14,36 @@ export const CadAltUbs = () => {
   const [dadosUbs, defineDadosUbs] = React.useState();
   const usuarioLogado = useRecoilValue(usuarioLogadoAtom);
   const navigate = useNavigate();
+  /**
+   * Usando essa variável de estado local pra forçar atualização do sistema de UI
+   * Outra forma de fazer isso seria usar `value` no lugar de `defaultValue`
+   * 
+   * O problema: Fazer isso precisaria que o sr aplicasse `onChange` em todos os Inputs
+   * Então dessa forma contorna esse problema
+   */
+  const [renderKey, setRenderKey] = React.useState();
 
-  const codMunic = usuarioLogado.codMunicipio;
-  const munic = usuarioLogado.municipio;
-  const uf = usuarioLogado.uf;
+  /**
+   * Fazendo isso para caso, por algum motivo, os dados do usuarioLogado ainda não estejam disponíveis
+   * Como um refresh, por exemplo
+   * Os dados são pegos no useMemo e voltam como um objeto
+   * E aí na linha inicial, o objeto é desconstruído
+   */
+  const { codMunic, munic, uf } = React.useMemo(() => {
+    if (!usuarioLogado?.uf) {
+      return {}
+    }
+    const codMunic = usuarioLogado.codMunicipio;
+    const munic = usuarioLogado.municipio;
+    const uf = usuarioLogado.uf;
+
+    return {
+      codMunic,
+      munic,
+      uf,
+    }
+
+  }, [usuarioLogado])
 
   React.useEffect(() => {
     const usuarioSessao = JSON.parse(sessionStorage.getItem('ubs-usuario'))
@@ -25,12 +51,12 @@ export const CadAltUbs = () => {
       navigate('/login');
     }
 
-    if (id) {
+    if (id && codMunic) {
       axios.get(URL_UBS, {
         params: {
           opc: 'buscaDadosUbs',
           codMunicipio: codMunic,
-          idUbs: id,
+          idUbs: Number(id),
         }
       }).then(response => {
         const novoRenderKey = gerarUid();
@@ -52,7 +78,7 @@ export const CadAltUbs = () => {
           toast.error(`Erro na requisição, verifique sua conexão. ${error}`)
         })
     }
-  }, []);
+  }, [id, codMunic]);
 
   const salvarDados = (dados) => {
     dados.preventDefault(); // para nao dar o refresh
@@ -86,43 +112,42 @@ export const CadAltUbs = () => {
       toast.error(erro);
     })
   }
- 
+
   return React.useMemo(() => {
-   
     return (
       <>
         <div className="titManutUbs">Cadastro da UBS</div>
         <form className='formCadAltUbs' onSubmit={salvarDados}>
           <div className='containerInputs'>
             <p>Nome:
-              <Input type="text" name="nome" value={dadosUbs?.nome} size="40" required />
+              <Input type="text" name="nome" defaultValue={dadosUbs?.nome} size="40" required key={renderKey} />
             </p>
             <p>Endereço:
-              <Input type="text" name="endereco" value={dadosUbs?.endereco} size="40" required />
+              <Input type="text" name="endereco" defaultValue={dadosUbs?.endereco} size="40" required key={renderKey} />
             </p>
             <p>Número:
-              <Input type="text" name="numero" defaultValue={dadosUbs?.numero} size="6" />
+              <Input type="text" name="numero" defaultValue={dadosUbs?.numero} size="6" key={renderKey} />
             </p>
             <p>Bairro:
-              <Input type="text" name="bairro" defaultValue={dadosUbs?.bairro} size="30" required />
+              <Input type="text" name="bairro" defaultValue={dadosUbs?.bairro} size="30" required key={renderKey} />
             </p>
             <p>Distrito:
-              <Input type="text" name="distrito" defaultValue={dadosUbs?.distrito} size="30" />
+              <Input type="text" name="distrito" defaultValue={dadosUbs?.distrito} size="30" key={renderKey} />
             </p>
             <p>Cidade:
-              <Input type="text" disabled name="municipio" size="30" defaultValue={munic} />
+              <Input type="text" disabled name="municipio" size="30" defaultValue={munic} key={renderKey} />
             </p>
             <p>UF:
-              <Input type="text" disabled name="uf" size="2" defaultValue={uf} />
+              <Input type="text" disabled name="uf" size="2" defaultValue={uf} key={renderKey} />
             </p>
             <p>CEP:
-              <Input type="tel" name="cep" defaultValue={dadosUbs?.cep} size="10" />
+              <Input type="tel" name="cep" defaultValue={dadosUbs?.cep} size="10" key={renderKey} />
             </p>
             <p>Contato:
-              <Input type="text" name="contato" defaultValue={dadosUbs?.contato} size="30" required />
+              <Input type="text" name="contato" defaultValue={dadosUbs?.contato} size="30" required key={renderKey} />
             </p>
             <p>Telefone Contato:
-              <Input type="text" name="telContato" defaultValue={dadosUbs?.telContato} size="16" required />
+              <Input type="text" name="telContato" defaultValue={dadosUbs?.telContato} size="16" required key={renderKey} />
             </p>
           </div>
           <div className="menuUbs">
@@ -138,6 +163,5 @@ export const CadAltUbs = () => {
         </form>
       </>
     )
-  
-  },[dadosUbs])
+  }, [dadosUbs]);
 }
